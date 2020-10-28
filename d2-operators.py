de235d9534878d7f436b1238e09e5abff6dd5db0
yjchen <yi-jen.chen@gmx.de>
2020-10-28 10:38:42 +0100

    complete day 2

---
+++ d2-operators.py
@@ -0,0 +1,19 @@
+# Description: https://www.hackerrank.com/challenges/30-operators/problem
+
+#import math
+#import os
+#import random
+#import re
+#import sys
+
+# Complete the solve function below
+def solve(meal_cost, tip_percent, tax_percent):
+    total_cost = meal_cost * (1 + tip_percent * 1e-2 + tax_percent * 1e-2)
+    print(round(total_cost))
+
+if __name__ == '__main__':
+    meal_cost = float(input())
+    tip_percent = int(input())
+    tax_percent = int(input())
+
+    solve(meal_cost, tip_percent, tax_percent)
