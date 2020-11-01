57fcc4f74e6108524d2c84e42617a9a7a5fe77db
yjchen <yi-jen.chen@gmx.de>
2020-11-01 19:36:20 +0100

    complete day 5

---
+++ d5-loops.py
@@ -0,0 +1,8 @@
+# Description: https://www.hackerrank.com/challenges/30-loops/problem
+
+if __name__ == '__main__':
+    n = int(input())
+    assert 2 <= n <= 20, 'Integer lies in invalid range'
+    
+    for i in range(1, 11):
+        print(f'{n} x {i} = {n * i}')
