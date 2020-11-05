4cacaf2438a2d63c1c7c0e5721d9947bfdb3fcbf
yjchen <yi-jen.chen@gmx.de>
2020-11-05 16:57:44 +0100

    update filenames

--- d08-dictionaries-and-maps.py
+++ d08-dictionaries-and-maps.py
@@ -1,13 +1,14 @@
 # Description: https://www.hackerrank.com/challenges/30-dictionaries-and-maps/problem
+import sys
 
 n = int(input())
-
+print(sys.stdin)
 phone_book = {}
 for i in range(n):
     name, phone = input().split()
     phone_book[name] = phone
 
-while True: 
+while True:
     try:
         q = input()
         if q in phone_book:
