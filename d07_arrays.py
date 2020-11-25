2e94027a1af37fa5d291b2988e77588d7658bfa8
yjchen <yi-jen.chen@gmx.de>
2020-11-25 10:35:08 +0100

    update d7 solution and filenames

--- d07_arrays.py
+++ d07_arrays.py
@@ -4,9 +4,10 @@ if __name__ == '__main__':
     n = int(input())
     assert 1 <= n <= 1000, 'Invalid integer'
     
-    arr = input().rstrip().split()
+    arr = list(map(int, input().rstrip().split()))
     assert len(arr) == n, 'Invalid number of array elements'
-    assert all(map(lambda x: 1 <= int(x) <= 10000, arr)), \
-           'Invalid array element(s)'
+    assert all(map(lambda x: 1 <= x <= 10000, arr)), \
+          'Invalid array element(s)'
     
-    print(' '.join(reversed(arr)))
+    for x in reversed(arr):
+        print(x, end=' ')
