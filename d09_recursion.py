07cd0cbb9f04726abb287f850f89e240d5dc8c94
yjchen <yi-jen.chen@gmx.de>
2020-11-26 10:25:57 +0100

    update d9 solution

--- d09_recursion.py
+++ d09_recursion.py
@@ -1,8 +1,9 @@
 # Description: https://www.hackerrank.com/challenges/30-recursion/problem
 
 def factorial(n):
-    assert n >= 0, 'Input lies in invalid range'
+    assert n >= 0, 'Invalid input'
     return 1 if n <= 1 else n * factorial(n-1)
+
 
 n = int(input())
 print(factorial(n))
