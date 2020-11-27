b3631cb8c432d3a31a3d96656bce225669f125dc
yjchen <yi-jen.chen@gmx.de>
2020-11-27 10:28:20 +0100

    update d10 solution

--- d10_binary-_numbers.py
+++ d10_binary-_numbers.py
@@ -1,11 +1,12 @@
 # Description: https://www.hackerrank.com/challenges/30-binary-numbers/problem
-# Note: can also use built-in `bin()` to convert decimal int to str binary
-#       representation
+# Notes:
+#   - can also use built-in `bin()` to convert decimal int to str binary rep
+#   - code works for nonnegative int
 
 n = int(input())
 
 n_bin = ''
-while n > 0:
+while n:
     n, b = divmod(n, 2)
     n_bin = str(b) + n_bin
     
