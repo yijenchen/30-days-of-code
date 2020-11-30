821d3783fffd3441d596828a2ba8450a1f321f37
yjchen <yi-jen.chen@gmx.de>
2020-11-30 15:20:28 +0100

    update d11 solution

--- d11_2d_arrays.py
+++ d11_2d_arrays.py
@@ -7,7 +7,7 @@ for _ in range(6):
 vals = []
 for i in range(4):
     for j in range(4):
-        val = sum(arr[i][j:j+3])+ arr[i+1][j+1] + sum(arr[i+2][j:j+3])
+        val = sum(arr[i][j:j+3]) + arr[i+1][j+1] + sum(arr[i+2][j:j+3])
         vals.append(val)
-            
+        
 print(max(vals))
