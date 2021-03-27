aabc3be7adf19d64ddcb681748470a3f222aebc4
yjchen <yi-jen.chen@gmx.de>
2021-03-27 06:24:23 +0100

    update d6 solution

--- d06_lets_review.py
+++ d06_lets_review.py
@@ -1,10 +1,7 @@
 # Description: https://www.hackerrank.com/challenges/30-review-loop/problem
 
 t = int(input())
-assert 1 <= t <= 10, 'Invalid integer'
 
 for i in range(t):
     s = input()
-    assert 2 <= len(s) <= 10000, 'Invalid string length'
-    
     print(s[::2], s[1::2])
