be17411097c70d2600985c9dd9cf17a0aad6bc66
yjchen <yi-jen.chen@gmx.de>
2021-03-30 12:17:08 +0200

    update d7 solution

--- d07_arrays.py
+++ d07_arrays.py
@@ -1,13 +1,8 @@
 # Description: https://www.hackerrank.com/challenges/30-arrays/problem
 
 if __name__ == '__main__':
-    n = int(input())
-    assert 1 <= n <= 1000, 'Invalid integer'
-    
+    _ = int(input())
     arr = list(map(int, input().rstrip().split()))
-    assert len(arr) == n, 'Invalid number of array elements'
-    assert all(map(lambda x: 1 <= x <= 10000, arr)), \
-          'Invalid array element(s)'
     
     for x in reversed(arr):
         print(x, end=' ')
